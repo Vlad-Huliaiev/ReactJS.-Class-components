cf9e8ee824600caf41ea27bd3b53522fa9c822cb
Vlad Huliaiev <gulyaev4420@gmail.com>
2020-09-10 23:39:37 +0300

    ReactJS.-Class-components added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
-//import Stories from './stories/stories.js';
 import Contacts from './contacts/contacts.js';
-//import './App.css';
 
 class App extends React.Component {
     render(){
