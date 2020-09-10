90862f5a2c1a5d1adf7a167635d59265cd72280f
Vlad Huliaiev <gulyaev4420@gmail.com>
2020-09-10 23:40:07 +0300

    Updates

---
+++ precache-manifest.be2cd0d39e73e0af8f3ccd287a092bf6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c781c0e6c9cea135f6ce04c6bd46071",
+    "url": "/ReactJS.-Class-components/index.html"
+  },
+  {
+    "revision": "bb3a165b7a01b1955408",
+    "url": "/ReactJS.-Class-components/static/css/main.ffd3998b.chunk.css"
+  },
+  {
+    "revision": "b5b61e29bfb4a9701834",
+    "url": "/ReactJS.-Class-components/static/js/2.940de9e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactJS.-Class-components/static/js/2.940de9e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb3a165b7a01b1955408",
+    "url": "/ReactJS.-Class-components/static/js/main.d9f3fed0.chunk.js"
+  },
+  {
+    "revision": "65ae3e94c8b710f294a4",
+    "url": "/ReactJS.-Class-components/static/js/runtime-main.76838e6e.js"
+  }
+]);
